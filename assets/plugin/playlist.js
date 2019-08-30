c6c0d6ae3a8a8da0628ec4eda385e97ae474cec6
brandinchiu <brandinchiu@penguin>
2019-08-30 11:33:42 -0400

    initial commit

---
+++ assets/plugin/playlist.js
@@ -0,0 +1,25 @@
+// JavaScript Document
+var playlist = [
+
+    {
+        mp3:'music/DoingMeWrong_MIXv1_16_441.mp3',
+        title:'Doing Me Wrong',
+        artist:'Debbie Fredericks',
+        duration:'04:04',
+        //cover:'mix/1.png'
+    },
+	{
+        mp3:'music/GOLDEN - instone.mp3',
+        title:'Golden',
+        artist:'Debbie Fredericks',
+        duration:'03:42',
+        //cover:'mix/1.png'
+    },
+	{
+        mp3:'music/RIBBON IN THE SKY-STEVIE WONDER-1.mp3',
+        title:'Ribbon in the Sky',
+        artist:'Stevie Wonder',
+        duration:'03:15',
+        //cover:'mix/1.png'
+    }
+];
